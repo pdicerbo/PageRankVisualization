5e57782445a6aa0c127ebac298415168bf87eb52
pdicerbo <pierluigi.dicerbo@gmail.com>
2016-06-05 12:17:16 +0200

    update MyScript.py and data input file

--- MyScript.py
+++ MyScript.py
@@ -24,7 +24,7 @@ for year in ["1990", "1995"]:
 
 MyStr = MyStr[:-1]
 MyStr += "]"
-# OutFile = open("OutDataSingle.json", "w")
-OutFile = open("OutData.json", "w")
+OutFile = open("OutDataSingle.json", "w")
+# OutFile = open("OutData.json", "w")
 OutFile.write(MyStr)
 OutFile.close()
